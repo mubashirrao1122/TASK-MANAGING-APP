661eff146e698110cf95cf8d0dad1e15e63961b1
MUBASHIR RAO <151456910+mubashirrao1122@users.noreply.github.com>
2025-04-19 13:14:07 +0500

    Add files via upload

---
+++ networks/QuoteResponse.java
@@ -0,0 +1,14 @@
+package com.example.taskmanagerapp.network;
+
+public class QuoteResponse {
+    private String q; // Quote text
+    private String a; // Author
+
+    public String getQuote() {
+        return q;
+    }
+
+    public String getAuthor() {
+        return a;
+    }
+}
